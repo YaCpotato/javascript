6397b5fcebf07d42380cd7a5576945c89527968e
Yasshi <47899653+YaCpotato@users.noreply.github.com>
2019-08-30 00:04:25 +0900

    Update gantt.js

--- gantt.js
+++ gantt.js
@@ -20,8 +20,16 @@
  var app = new Vue({
   el:'#app',
   data:{
-    tasks : [],
-    id:'',
+    tasks : [
+      {
+        id: 0,
+        name: 'my name',
+        start: '2016-12-28',
+        end: '2016-12-31',
+        progress: 20,
+      },
+    ],
+    id:null,
     name:'',
     start:'',
     end:''
@@ -35,10 +43,31 @@
         end: '2016-12-31',
         progress: 20,
       })
-      var gantt = new Gantt("#gantt", this.tasks);
-    }
-  }
+      gantt.refresh(this.tasks)
+    },
+  },
 })
+
+var gantt = new Gantt("#gantt", app.tasks,{
+        on_date_change: function(task, start, end) {
+           for(var i=0;i<gantt.tasks.length;i++){
+             if(app.tasks[i].id == task.id){
+               console.log('判定')
+               var item={
+                 name:task.name,
+                 start : moment(start).format('YYYY-MM-DD'),
+                 end : moment(end).format('YYYY-MM-DD'),
+               }
+               app.tasks.splice(i,1,item)
+               break;
+             }
+             break;
+           }
+        },
+        on_progress_change: function(task, progress) {
+            console.log(task, progress);
+        },
+ });
 </script>
 </body>
 </html>
